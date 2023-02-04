e103ebec6e6949af9d64970be6ad003b6018af37
Joshua Liu <joshualiu500@gmail.com>
2023-02-03 22:24:24 -0800

    pwa part 2

--- test_pwa/serviceWorker.js
+++ test_pwa/serviceWorker.js
@@ -1,26 +1,26 @@
-const venmo_clone = "venmo_clone"
-const assets = [
+const cacheName = "venmo_clone"
+const filesToCache = [
   "/",
   "/index.html",
   "/index.css",
-  "/venmo_logo.png",
+  "/main.js",
   "/payment/payment.html",
   "/payment/payment.css",
   "/payment/venmo_template.jpg"
 ]
 
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(venmo_clone).then(cache => {
-      cache.addAll(assets)
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
     })
-  )
-})
+  );
+});
 
-self.addEventListener("fetch", fetchEvent => {
-    fetchEvent.respondWith(
-      caches.match(fetchEvent.request).then(res => {
-        return res || fetch(fetchEvent.request)
-      })
-    )
-  })
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
