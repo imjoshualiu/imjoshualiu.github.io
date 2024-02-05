c6ff1ea607a0413b49b6bf3764e6cc0ee46f7a45
Joshua Liu <joshualiu500@gmail.com>
2024-02-04 17:24:02 -0800

    multiple model support

--- test_pwa/main.js
+++ test_pwa/main.js
@@ -23,10 +23,16 @@ if (form) {
       const receiver_user = document.getElementById("receiver_user").value;
       const amt = document.getElementById("amt").value;
       const desc = document.getElementById("desc").value;
+      const phone_model = document.getElementById("phone_model").value;
       dataArray.push({ picture, receiver, receiver_user, amt, desc });
       console.log("Data: ", dataArray);
       window.sessionStorage.setItem("data",JSON.stringify(dataArray)); // session storage values must be strings, so stringify it
-      window.location.href = './payment/payment.html';
+      if (phone_model == "reg"){
+        window.location.href = './payment/payment_reg.html';
+      }
+      else{
+        window.location.href = './payment/payment.html';
+      }
     });
 }
 else {
